10ed9026c0ae66a44a8b6b2cbd2e600c630d6975
Júlia Bassul <jubassul@gmail.com>
2021-10-15 18:24:30 -0300

    updating transaction function

--- script.js
+++ script.js
@@ -47,15 +47,44 @@ const transactions =
 // 3) entrada - saídas = total 
 
 const Transaction = {
-    incomes() {
-        //funcionalidade de somar as entradas
+    //criou um atalho p/ todas as transacoes
+    all: transactions,
+    add(transaction){
+Transaction.all.push(transaction)
+App.reload()
     },
+    //funcionalidade de somar as entradas
+    incomes() { 
+        let income = 0;
+        //pegar todas as transacoes
+        //para cada transacao, 
+        Transaction.all.forEach(transactions =>{
+        //se ela for maior que zero
+        if(transactions.amount > 0) {
+        //somar a uma variavel e retornar a variavel
+        income = income + transactions.amount
+        }
+        })
+        return income;
+    },
+    //funcionalidade de somar as saídas
     expenses() {
-        //funcionalidade de somar as saídas
+        let expense = 0;
+        //pegar todas as transacoes
+        //para cada transacao, 
+        Transaction.all.forEach(transactions =>{
+        //se ela for menor que zero
+        if(transactions.amount < 0) {
+        //somar a uma variavel e retornar a variavel
+        expense = expense + transactions.amount
+        }
+        })
+        return expense;  
     },
+    //funcionalidade de total (entrada-saídas)
     total() {
-        //funcionalidade de total (entrada-saídas)
-    },
+    return Transaction.incomes() + Transaction.expenses()
+    }
 }
 
 //eu preciso pegar os dados da table que estão no objeto e colocar lá no html: irei substituir os dados da table do html com os dados do js//
@@ -68,19 +97,80 @@ const DOM = {
         DOM.transactionsContainer.appendChild(tr)
     },
     innerHTMLTransaction(transaction) {
+        //se transaction.amount for menor que zero, será income, se não for, será expense, na tela: income ficará vermelho e expense verde, coloquei essa variável, interpolada na linha do html, onde aparece os números
+        const CSSclass = transaction.amount > 0 ? "income" : "expense"
+        const amount = Utils.formatCurrency(transaction.amount)
         const html = `
      <td class="description">${transaction.description}</td>
-     <td class="expense">${transaction.amount}</td>
+    <td class="${CSSclass}">${amount}</td> 
      <td class="date">${transaction.date}</td>
      <td>
          <img src="arquivos/minus.svg">
      </td>
  `
-        console.log(html)
-
         return html
-    }
+    },
+    
+    //peguei o id da tag p do html e estou imprindo na tela, usando innerHTML
+
+updateBalance() {
+    document
+    .getElementById('incomesDisplay') 
+    .innerHTML = Utils.formatCurrency(Transaction.incomes())
+    document
+    .getElementById('expensesDisplay') 
+    .innerHTML = Utils.formatCurrency(Transaction.expenses())
+    document
+    .getElementById('totalDisplay')
+    .innerHTML = Utils.formatCurrency(Transaction.total()) 
+
+  },
+  clearTransactions() {
+     DOM.transactionsContainer.innerHTML = "" 
+  }
 }
-DOM.addTransaction(transactions[0])
-DOM.addTransaction(transactions[1])
-DOM.addTransaction(transactions[2])
+
+
+//funcionalidade para o sinal do número (- ou nada)
+const Utils = {
+formatCurrency(value){
+//se o valor for medor que zero, entao retorna -, se não for, não irá retornar nada, ou seja, ficará sem o sinal
+const signal = Number(value) < 0 ? "-" : ""
+
+value = String(value).replace(/\D/g, "")
+value = Number(value / 100)
+//abaixo temos uma funcionalidade para transformar em moeda, primeiro arg a localização e o segundo as opções como objeto: style(currency-moeda) e currency-brl o tipo da moeda
+value = value.toLocaleString("pt-BR", {
+style: "currency",
+currency: "BRL"
+})
+return signal +value
+}
+}
+
+//para cada elemento será executado, como argumento, uma funcionalidade
+//enquanto o i for menor que 3, isso irá se repetir.
+//for(let i =0; i<3; i++){
+
+//} o forEach só é uma outra forma de escrever o que está acima
+const App={
+    init() {
+Transaction.all.forEach(transaction => {
+DOM.addTransaction(transaction)
+})
+DOM.updateBalance()
+    },
+reload() {
+    DOM.clearTransactions()
+    App.init()
+},
+}
+App.init()
+
+
+Transaction.add({
+    id: 39,
+description: 'oi',
+amount: 200,
+date: '23/02/2020',
+})
